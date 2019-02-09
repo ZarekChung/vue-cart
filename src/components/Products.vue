0e19cec1fdea6d873c0c441f79f9bff5ce04abbb
Zarek <simen1127@gmail.com>
2019-02-09 14:18:28 +0800

    add currency filter

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -18,8 +18,8 @@
         <tr>
           <td>{{item.category}}</td>
           <td>{{item.title}}</td>
-          <td>{{item.origin_price}}</td>
-          <td>{{item.price}}</td>
+          <td>{{item.origin_price | currency}}</td>
+          <td>{{item.price | currency}}</td>
           <td>
             <span v-if="item.is_enabled" class="text-success">啟用</span>
             <span v-else>未啟用</span>
